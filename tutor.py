5be7eeeb68a1d81a2f8b3f021edb7b2813b087df
afr2903 <afr102903@gmail.com>
2023-12-19 13:29:22 -0600

    Streaming prompt

--- tutor.py
+++ tutor.py
@@ -10,7 +10,36 @@ client = OpenAI(
 audio_file = open("input-audio/1.wav", "rb")
 transcript = client.audio.transcriptions.create(
     model="whisper-1",
-    file=audio_file
+    file=audio_file,
+    language="es",
+    response_format="text"
 )
 
 print(transcript)
+
+prompt = [
+    {
+        "role": "system",
+        "content": "You are a virtual math tutor for a web platform focused on middle school students.You will receive a question asked by an student about a math problem and you will provide an easy to understand reasoning towards the solution. Avoid giving the answer directly, instead, try to guide the student towards the solution."
+    },
+    {
+        "role": "user",
+        "content": transcript
+    }
+]
+
+stream = client.chat.completions.create(
+    model="gpt-4",
+    messages=prompt,
+    stream=True,
+)
+response = ""
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="|", flush=True)
+        response += chunk.choices[0].delta.content
+
+print()
+
+print(response)
